2d2299c752fe387799391ffc45115627d6a12bd3
x-bird <x-bird@qiubs.com>
2013-09-10 16:09:28 +0800

    实力代码继续完成 about body widget

---
+++ company/widgets/body/js/init-body.js
@@ -0,0 +1,12 @@
+define(function(require, exports, module) {
+
+	var $ = require('$');
+
+	var complain = require('my-util');
+
+	complain();
+	complain(100000000);
+	
+	seajs.log('loaded init-body~~');
+
+});
